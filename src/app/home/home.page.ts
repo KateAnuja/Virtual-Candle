10c128f84f6d640683f07abf1796cc30447637cf
Anuja Kate <kate.anuja15@gmail.com>
2021-05-10 18:56:59 +0530

    live audio stream from mic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-
+declare var MediaRecorder: any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  constructor(
+  ) {}
+  
+  ionViewWillEnter(){
+    navigator.mediaDevices.getUserMedia({ audio: true })
+    .then(stream => {
+      
+      const mediaRecorder = new MediaRecorder(stream);
+      mediaRecorder.start(300);
+      console.log("recording")
+      mediaRecorder.addEventListener("dataavailable", e => {
+        const blobDataInWebaFormat = e.data; // .weba = webaudio; subset of webm
+        const blobDataInWavFormat: Blob = new Blob([blobDataInWebaFormat], { type : 'audio/wav; codecs=0' });
+        var reader = new FileReader();
+          reader.readAsDataURL(blobDataInWavFormat);
+          reader.onloadend = function () {
+          var base64String = reader.result;
+          console.log('Base64 String - ', base64String);
+        }
+      });
+    });
 
-  constructor() {}
+    // this.mediaCapture.captureAudio()
+    // .then((x)=>{
+    //   console.log(x);
+    // })
+  }
+
+  ionViewDidEnter(){
+    
+  }
+
+  ionViewWillLeave(){
+    
+  }
+
+  ionViewDidLeave(){
+
+  }
 
 }
